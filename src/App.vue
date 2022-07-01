6bdc71401e313414d9e66ce731ff1c2fa04925d7
Koen Stuurman <koenstuurman@gmail.com>
2022-07-01 17:06:31 +0200

    met links naar andere paginas maar zonder inhoud somehow

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
+
 <template>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
   <router-view />
 </template>
 
 <script>
+
 import { defineComponent } from 'vue'
 
 export default defineComponent({
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,7 +7,22 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
-
+  {
+    path: '/ToegewezenRapportages',
+    component: () => import('pages/ToegewezenRapportages.vue')
+  },
+  {
+    path: '/UitgevoerdeRapportages',
+    component: () => import('pages/UitgevoerdeRapportages.vue')
+  },
+  {
+    path: '/DocumentatiePage',
+    component: () => import('src/pages/DocumentatiePage.vue')
+  },
+  {
+    path: '/InstellingenPage',
+    component: () => import('src/pages/InstellingenPage.vue')
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
