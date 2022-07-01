1fa526daef611e69de5987df5733addc6eeb4e76
Koen Stuurman <koenstuurman@gmail.com>
2022-07-01 15:44:58 +0200

    met buttons zonder link

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,10 +2,38 @@
   <q-page class="flex flex-center">
     <div class="centerPageLogo">
     <img
-      alt="Quasar logo"
+      alt="Real Estate Care Logo"
       src="~assets/logo.png"
       style="width: 200px; height: 200px"
     ></div>
+
+<div id="q-app" style="min-height: 100vh;">
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn class="button1"
+      padding="lg"
+      color="primary"
+      icon="eco"
+    >Toegewezen Rapportages</q-btn>
+
+     <q-btn class="button2"
+      padding="lg"
+      color="primary"
+      icon="eco"
+    >Uitgevoerde Rapportages</q-btn>
+
+     <q-btn class="button3"
+      padding="lg"
+      color="primary"
+      icon="eco"
+    >Kennisbase</q-btn>
+
+     <q-btn class="button4"
+      padding="lg"
+      color="primary"
+      icon="eco"
+    >Instellingen</q-btn>
+  </div>
+</div>
   </q-page>
 </template>
 
